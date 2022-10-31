3f49f4b5fc5d365cfdd760eaa901339c3bdad277
lavaspoon <lavaspoon@naver.com>
2022-10-31 23:01:18 +0900

    셀 xib 생성

---
+++ DrinkWater/AlertListCell.swift
@@ -0,0 +1,23 @@
+//
+//  AlertListCell.swift
+//  DrinkWater
+//
+//  Created by spoon lava on 2022/10/31.
+//
+
+import UIKit
+
+class AlertListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ DrinkWater/AlertListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AlertListViewController.swift
+//  DrinkWater
+//
+//  Created by spoon lava on 2022/10/31.
+//
+
+import UIKit
+
+class AlertListViewController: UITableViewController {
+    @IBAction func addAlertButtonAction(_ sender: UIBarButtonItem) {
+        
+    }
+}
